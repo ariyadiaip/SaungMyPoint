340978cbc0d1b46a86f195bd7441f76a53022de7
ariyadiaip <ariyadiaip@gmail.com>
2019-08-31 17:55:32 +0700

    Adding UI V2

---
+++ app/src/main/java/id/sch/bdg/smkn4/pkk/saung_mypoint/AddReportActivity.java
@@ -0,0 +1,13 @@
+package id.sch.bdg.smkn4.pkk.saung_mypoint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_report);
+    }
+}
